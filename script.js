f80d8c1560e446352c393939806a02550ff41d19
Jake Boyce <thisisjakeboyce@gmail.com>
2019-02-16 21:59:08 +0000

    Implement clear/resize button

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const container = document.querySelector(".container");
-let numOfColumns = 16;
-let numOfRows = 16;
+let numOfSquares = 16;
 
-container.setAttribute("style", "grid-template: repeat(" + numOfColumns + 
-                        ", 1fr) / repeat(" + numOfRows + ", 1fr);");
 
-for(let i=0; i < numOfColumns; i++) {
-  for(let j=0; j < numOfRows; j++) {
-    const box = document.createElement("div");
-    box.setAttribute("class", "box");
-    container.appendChild(box);
+renderGrid(numOfSquares);
+
+const button = document.querySelector("button");
+button.addEventListener("click", e => {
+  clearGrid();
+  renderGrid(numOfSquares);
+});
+
+function clearGrid() {
+  const boxes = document.querySelectorAll(".box");
+  for (let i = 0; i < boxes.length; i++) {
+    container.removeChild(boxes[i]);
   }
+  numOfSquares = parseInt(prompt("How many squares per side should the new grid be?", ""));
 }
 
-const boxes = document.querySelectorAll(".box");
-boxes.forEach(box => {
-  box.addEventListener("mouseover", e => {
-    e.target.style.background = "black";
-  })
-})
+function renderGrid(num) {
+  container.setAttribute("style", "grid-template: repeat(" + num + 
+                        ", 1fr) / repeat(" + num + ", 1fr);");
+  for(let i=0; i < num; i++) {
+    for(let j=0; j < num; j++) {
+      const box = document.createElement("div");
+      box.setAttribute("class", "box");
+      box.style.width = `${960 / num}px`;
+      box.style.height = `${960 / num}px`;
+      container.appendChild(box);
+    }
+  }
+
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach(box => {
+    box.addEventListener("mouseover", e => {
+      e.target.style.background = "black";
+    });
+  });
+}
